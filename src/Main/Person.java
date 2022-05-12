0770cb499a1c989854c22d15dd60741e20f650fb
Aaron <aaronmoyaarques@gmail.com>
2022-05-12 18:16:51 +0200

    1.0

--- src/Main/Person.java
+++ src/Main/Person.java
@@ -1,5 +1,9 @@
 package Main;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Person {
     protected  String dni;
     protected  String correo;
@@ -14,7 +18,16 @@ public class Person {
     }
 
     public void setDni(String dni) {
-        this.dni = dni;
+        Pattern REGEXP = Pattern.compile("[0-9]{8}[A-Z]");
+        String DIGITO_CONTROL = "TRWAGMYFPDXBNJZSQVHLCKE";
+        String[] INVALIDOS = new String[] { "00000000T", "00000001R", "99999999R" };
+        boolean correcto = false;
+        correcto = Arrays.binarySearch(INVALIDOS, dni) < 0
+                && REGEXP.matcher(dni).matches()
+                && dni.charAt(8) == DIGITO_CONTROL.charAt(Integer.parseInt(dni.substring(0, 8)) % 23);
+
+        if (correcto) this.dni = dni;
+        else this.dni = "";
     }
 
     public String getCorreo() {
@@ -22,7 +35,15 @@ public class Person {
     }
 
     public void setCorreo(String correo) {
-        this.correo = correo;
+        String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(correo);
+        boolean correcto = false;
+        correcto = matcher.matches();
+        if (correcto) this.correo = correo;
+        else this.correo = "";
     }
 
     @Override
--- tests/Main/PersonTest.java
+++ tests/Main/PersonTest.java
@@ -4,12 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class PersonTest {
     Person p;
     protected String dni;
@@ -24,6 +18,7 @@ class PersonTest {
 
     @AfterEach
     void tearDown() {
+
     }
 
     @Test
@@ -32,17 +27,8 @@ class PersonTest {
     }
 
     @Test
-    void setDni(String dni) {
-        Pattern REGEXP = Pattern.compile("[0-9]{8}[A-Z]");
-        String DIGITO_CONTROL = "TRWAGMYFPDXBNJZSQVHLCKE";
-        String[] INVALIDOS = new String[] { "00000000T", "00000001R", "99999999R" };
-        boolean correcto = false;
-        correcto = Arrays.binarySearch(INVALIDOS, dni) < 0
-                && REGEXP.matcher(dni).matches()
-                && dni.charAt(8) == DIGITO_CONTROL.charAt(Integer.parseInt(dni.substring(0, 8)) % 23);
+    void setDni() {
 
-        if (correcto) this.dni = dni;
-        else this.dni = "";
 
     }
 
@@ -53,16 +39,8 @@ class PersonTest {
     }
 
     @Test
-    void setCorreo(String correo) {
-        String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+    void setCorreo() {
 
-        Pattern pattern = Pattern.compile(regex);
-
-        Matcher matcher = pattern.matcher(correo);
-        boolean correcto = false;
-        correcto = matcher.matches();
-        if (correcto) this.correo = correo;
-        else this.correo = "";
     }
 
     @Test
